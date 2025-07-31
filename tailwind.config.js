5fad699216b34e6ca4d49930138fe472e2827223
iamgouravmukherjee <gouravmukherjee09@gmail.com>
2025-07-31 04:46:14 -0400

    shadcn and storybook setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}", // If you chose src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
